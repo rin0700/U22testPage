8ed84ad25cf62a8509791f4071db61f82d13b884
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-05-21 22:31:19 +0900

    ReactFile作成・Headerの追加

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import '../style/Style.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div class="header-logo">
+        {/* ロゴ画像を追加 */}
+        <a href="http://localhost//Cafe%20dolceCalmato/index.php">
+          {/* ロゴがクリックされた時のリンク先を指定 */}
+          <img src="../cafe_logo.png" alt="Cafe Logo" class="logo-image" />
+        </a>
+
+        <h1 id="page-top" class="font-1">
+          Cafe dolce＆calmato
+        </h1>
+      </div>
+
+      <nav>
+        <ul class="menu-bar">
+          <li>
+            <h2 class="font-2">
+              <a href="#home">Home</a>
+            </h2>
+          </li>
+          <li>
+            <h2 class="font-2">
+              <a href="http://localhost//Cafe%20dolceCalmato/index.php#menu">
+                Menu
+              </a>
+            </h2>
+          </li>
+          <li>
+            <h2 class="font-2">
+              <a href="http://localhost//Cafe%20dolceCalmato/TartOrder.php">
+                Shop
+              </a>
+            </h2>
+          </li>
+          <li>
+            <h2 class="font-2">
+              <a href="#contact">Contact</a>
+            </h2>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
